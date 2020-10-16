f0f9cf9bd317f92b3b38f1be8fccd012cd36cb7f
redwoodcutter <oguzkumcular@gmail.com>
2020-10-16 22:38:40 +0300

    HTTP REQUEST WİTH AXİOS

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+//import axios from 'axios';
+import axios from '../../axios';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
+
+    //Getting Json File
+    componentDidMount(){
+     axios.get('/posts')
+        .then(response =>{
+            const posts = response.data.slice(0,4);
+            const updatedPost = posts.map(post=>{
+                return{
+                    ...post,
+                    author: 'Cezmi'
+                }
+            })
+            this.setState({posts: updatedPost});
+            // console.log(response);
+        });
+    }
+    //Selected id function
+    postSelectedHandler = (id) =>{
+        this.setState({selectedPostId: id});
+    }
+
+    render () {
+        const posts = this.state.posts.map(
+            post=>{
+                return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author}
+                clicked={()=>this.postSelectedHandler(post.id)}/>
+            }
+        );
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPostId} /> 
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
